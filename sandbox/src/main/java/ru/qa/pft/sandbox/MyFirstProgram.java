c715367e0676ee0fcb951662275513b00b455e34
Eugene-zaretski <zaretski25@gmail.com>
2020-07-29 21:09:12 +0300

    первые методы и классы

--- sandbox/src/main/java/ru/qa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,11 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, word");
-      System.out.println(2 *2 );
+        System.out.println(2 * 2);
+
+        Squar s = new Squar(6);
+        System.out.println("Площадь квадрата = " + s.area());
 
     }
+
 }
---
+++ sandbox/src/main/java/ru/qa/pft/sandbox/Squar.java
@@ -0,0 +1,13 @@
+package ru.qa.pft.sandbox;
+
+public class Squar {
+    public double l;
+
+    public Squar(double l) {
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
